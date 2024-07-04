d0511d77fa3d6af7802fda5bd4bc98f0bffaf141
naresh7km <naresh7km@gmail.com>
2024-07-04 12:52:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const firstList = ["shibuyahotel", "yuuyuuyoga", "massagesayami", "greenshousejapanesefoodtruck", "greenshousemomostruck", "tan-nenspa"];
-const secondList = ["hisashieats", "hanamionsen.site", "goelectricjapan", "yuugenochiru", "komorebiyoga"];
+const secondList = ["hisashieats", "hanamionsen.site", "harmonygreentea", "yuugenochiru", "komorebiyoga"];
 const thirdList = ["sakurasuhiandramenbar"];
 const fourthList = ["hdjavforyou"];
 
@@ -25,7 +25,7 @@ const baseDomains = [
   "hdjavforyou.online",
   "greenshousemomostruck.com",
   "hanamionsen.site",
-  "goelectricjapan.life",
+  "harmonygreentea.com",
 ];
 
 const generateAllowedUrls = (domains) => {
