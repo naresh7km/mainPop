1f00d6acbc759a746bf761d9b0ca87aee9d9a624
naresh7km <naresh7km@gmail.com>
2024-09-11 10:36:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const firstList = ["yuuyuuyoga", "yumikoresorts", "sakurasuhiandramenbar", "hanasakuniwa"];
-const secondList = ["spa-delightjp", "komorebiyoga", "shoppiejapanclothes", "yogametic"];
-const thirdList = ["seishinyogajp", "tatsumionsen"];
+const secondList = ["komorebiyoga", "shoppiejapanclothes", "yogametic"];
+const thirdList = ["spa-delightjp", "seishinyogajp", "tatsumionsen"];
 const fourthList = ["hdjavforyou", "javhdaffiliates"];
 const fifthList = ["hanamionsen", "wash-o-kuan", "yogalex", "shinkansen", "sakuracoffee", "hikokimagic", "yumimasseuse", "fitzonefitness", "osakastreetbites"];
 
