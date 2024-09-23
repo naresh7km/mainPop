86198bcc4abcd5089c7e73283b64ca85db063f19
naresh7km <naresh7km@gmail.com>
2024-09-23 15:04:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 const firstList = ["yuuyuuyoga", "yumikoresorts", "sakurasuhiandramenbar", "hanasakuniwa"];
 const secondList = ["shinkansen"];
 const thirdList = ["spa-delightjp", "seishinyogajp", "tatsumionsen", "yogametic", "foodhutjapan", "hotelroyaltonjp"];
-const fourthList = ["hdjavforyou", "javhdaffiliates", "shoppiejapanclothes", "osakastreetbites", "hikokimagic", "fitzonefitness"];
-const fifthList = ["sakuracoffee", "yumimasseuse"];
+const fourthList = ["hdjavforyou", "javhdaffiliates", "shoppiejapanclothes", "hikokimagic", "fitzonefitness"];
+const fifthList = ["sakuracoffee", "yumimasseuse", "osakastreetbites"];
 // Base domains for allowed origins and referrers
 const baseDomains = [
   "sakurasuhiandramenbar.com",
