5aea93ccbf5fc2e054699af15445dda3570d1935
naresh7km <naresh7km@gmail.com>
2024-10-01 11:31:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const firstList = ["yuuyuuyoga", "sakurasuhiandramenbar", "shinkansenfacts", "hikokifacts", "shoppiejapaneseclothes", "mahbubanioldage", "osakastreetbites"];
-const secondList = ["spadelightjapan"];
+const firstList = ["yuuyuuyoga", "sakurasuhiandramenbar", "hikokifacts", "shoppiejapaneseclothes", "mahbubanioldage", "osakastreetbites"];
+const secondList = ["spadelightjapan", "shinkansenfacts"];
 const thirdList = ["seishinyogajp", "foodhutjapan", "hotelroyaltonjp"];
 const fourthList = ["hdjavforyou", "javhdaffiliates", ];
 const fifthList = [];
