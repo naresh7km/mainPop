b4e68350b7fa1fb0c5953a61add2af50f91ce66a
naresh7km <naresh7km@gmail.com>
2024-10-01 12:02:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const firstList = ["yuuyuuyoga", "sakurasuhiandramenbar", "hikokifacts", "shoppiejapaneseclothes", "mahbubanioldage", "osakastreetbites"];
+const firstList = ["yuuyuuyoga", "sakurasuhiandramenbar", "hikokifacts"];
 const secondList = ["spadelightjapan", "shinkansenfacts"];
 const thirdList = ["seishinyogajp", "foodhutjapan", "hotelroyaltonjp"];
 const fourthList = ["hdjavforyou", "javhdaffiliates", ];
-const fifthList = [];
+const fifthList = ["shoppiejapaneseclothes", "mahbubanioldage", "osakastreetbites"];
 const sixthList = ["fitzonefitness"];
 
 // Base domains for allowed origins and referrers
