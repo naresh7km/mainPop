8b90d0b4db302b4d7c6aadf87a1c445d01729e5c
naresh7km <naresh7km@gmail.com>
2024-10-02 12:27:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const sixthList = ["fitzonefitness", "wanoiyashi", "fitclubgathering"];
 const baseDomains = [
   "sakurasuhiandramenbar.com",
   "foodhutjapan.com",
-  "hotelroyaltonjp.com",
+  "hotelroyaltonjp.club",
   "spadelightjapan.fit",
   "javhdaffiliates.site",
   "seishinyogajp.fit",
