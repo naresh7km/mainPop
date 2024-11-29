6a254af477855fb6f5caf6bb90a0d8118fb5e4ec
naresh7km <naresh7km@gmail.com>
2024-11-29 06:02:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const firstList = ["spadelightjapan"];
+const firstList = ["spadelightjapan", "seishinyoga", "yuyado-onsen", "foodhutjapan"];
 const secondList = ["hdjavforyou", "javhdaffiliates"];
-const thirdList = ["seishinyoga", "yuyado-onsen", "foodhutjapan"];
+const thirdList = [];
 const fourthList = ["tanenespa"];
 const fifthList = ["ruijiacoaching", "calmyogaandmeditation"];
 const sixthList = ["nursecares", "expressmealdelivery", "yogalhealing", "gourmethub", "omnifoodss"];
