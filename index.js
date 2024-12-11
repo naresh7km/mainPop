39e7231b939c85d974c8e593705557e835a35df5
naresh7km <naresh7km@gmail.com>
2024-12-11 09:19:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const firstList = ["spadelightjapan", "komorebiyoga", "tanenespa"];
-const secondList = ["hdjavforyou", "javhdaffiliates", "omnifoodss"];
+const secondList = ["hdjavforyou", "javhdaffiliates"];
 const thirdList = ["seishinyoga", "yuyado-onsen", "foodhutjapan"];
 const fourthList = [];
 const fifthList = ["ruijiacoaching", "calmyogaandmeditation", "gardenretreatspa", "oceanrestaurant", "kokoroyoga"];
-const sixthList = ["nursecares", "expressmealdelivery", "yogalhealing", "gourmethub"];
+const sixthList = ["nursecares", "expressmealdelivery", "yogalhealing", "gourmethub", "omnifoodss"];
 
 // Base domains for allowed origins and referrers
 const baseDomains = [
