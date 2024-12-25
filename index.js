4e66851e79eaec08d1bc0c8858dd8d5ded7d4ac3
naresh7km <naresh7km@gmail.com>
2024-12-25 19:00:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const aomineList3 = ["nursecares", "expressmealdelivery", "yogalhealing", "gourm
 const aomineList4 = [];
 const aomineList5 = [];
 
-const wayneList1 = ["ruijiacoaching.onrender.com", "delightful-smakager-b5ed06.netlify.app", "oceanrestaurant.netlify.app", "kokoroyogaonline.onrender.com", "goldenflowerhotel.onrender.com", "matsucafe.onrender.com", "summerlighthotel.onrender.com"];
+const wayneList1 = ["ruijiacoaching.onrender.com", "delightful-smakager-b5ed06.netlify.app", "oceanrestaurant.netlify.app", "kokoroyogaonline.onrender.com", "goldenflowerhotel.netlify.app", "matsucafe.onrender.com", "summerlighthotel.onrender.com"];
 const wayneList2 = [];
 const wayneList3 = [];
 const wayneList4 = [];
@@ -49,7 +49,7 @@ const baseDomains = [
   "oceanrestaurant.netlify.app",
   "kokoroyogaonline.onrender.com",
   "delightful-smakager-b5ed06.netlify.app",
-  "goldenflowerhotel.onrender.com",
+  "goldenflowerhotel.netlify.app",
   "matsucafe.onrender.com",
   "maindomainn.netlify.app",
   "summerlighthotel.onrender.com",
