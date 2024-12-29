591dd8bbb78e59b355412a978529b51388906131
naresh7km <naresh7km@gmail.com>
2024-12-29 18:22:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const officeList5 = [];
 
 const dmcList1 = ["seishinyoga", "tanenespa"];
 const dmcList2 = ["komorebiyoga"];
-const dmcList3 = [];
+const dmcList3 = ["yoga-center"];
 const dmcList4 = [];
 const dmcList5 = [];
 
@@ -35,6 +35,7 @@ const baseDomains = [
   "tanenespa.onrender.com",
   "komorebiyoga.onrender.com",
   "javhdaffiliates.site",
+  "yoga-center.onrender.com",
   "seishinyoga.com",
   "hdjavforyou.online",
   "yumimasseuse.shop",
