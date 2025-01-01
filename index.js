ddf6d3e948cc56a1efe74ba04ef238e9ce7521ab
naresh7km <naresh7km@gmail.com>
2025-01-01 18:53:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const aomineList3 = ["nursecares", "expressmealdelivery", "yogalhealing", "gourm
 const aomineList4 = [];
 const aomineList5 = [];
 
-const wayneList1 = ["delightful-smakager-b5ed06.netlify.app", "kokoroyoga.netlify.app", "goldenflowerhotel.netlify.app", "matsucafe.onrender.com"];
-const wayneList2 = ["oceanrestaurant.netlify.app"];
+const wayneList1 = ["oceanrestaurant.netlify.app", "delightful-smakager-b5ed06.netlify.app", "kokoroyoga.netlify.app", "goldenflowerhotel.netlify.app", "matsucafe.onrender.com"];
+const wayneList2 = [];
 const wayneList3 = [];
 const wayneList4 = [];
 const wayneList5 = [];
