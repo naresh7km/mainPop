365a8dcf3ab2edde745b41359bd8099bb6827b08
naresh7km <naresh7km@gmail.com>
2025-01-08 05:12:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const dmcList3 = ["tanenespa"];
 const dmcList4 = [];
 const dmcList5 = [];
 
-const aomineList1 = ["expressmealdelivery", "yogalhealing", "gourmethub", "maindomainn.netlify.app", "omniifoodss", "nursecares"];
+const aomineList1 = ["expressmealdelivery", "yogalhealing", "gourmethub", "maindomainn.netlify.app", "newomnifoodss", "nursecares"];
 const aomineList2 = [];
 const aomineList3 = [];
 const aomineList4 = [];
@@ -40,7 +40,7 @@ const baseDomains = [
   "yumimasseuse.shop",
   "expressmealdelivery.shop",
   "hanasakuniwa.shop",
-  "omniifoods.shop",
+  "newomnifoodss.shop",
   "gourmethubeats.shop",
   "yogahealing.netlify.app",
   "nursecares.netlify.app",
