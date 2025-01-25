e6d91ed3d0e2e79183400a30e179713021403382
naresh7km <naresh7km@gmail.com>
2025-01-25 10:35:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const dmcList3 = ["tanenespa", "komorebiyoga"];
 const dmcList4 = [];
 const dmcList5 = [];
 
-const aomineList1 = [];
+const aomineList1 = ["expressmealdelivery", "newomnifoodss", "nursefood"];
 const aomineList2 = [];
-const aomineList3 = ["expressmealdelivery", "newomnifoodss", "nursefood"];
+const aomineList3 = [];
 const aomineList4 = [];
 const aomineList5 = [];
 
