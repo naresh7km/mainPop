cb61f3cba460634c6189d0944f7336615f64149e
naresh7km <naresh7km@gmail.com>
2025-02-07 13:02:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const aomineList4 = [];
 const aomineList5 = [];
 
 // for mumbai - list 1
-const wayneList1 = [];
+const wayneList1 = ["oceanrestaurant.netlify.app", "delightful-smakager-b5ed06.netlify.app", "matsucafe.onrender.com"];
 // for office - list 2
-const wayneList2 = ["oceanrestaurant.netlify.app", "delightful-smakager-b5ed06.netlify.app", "matsucafe.onrender.com"];
+const wayneList2 = [];
 
 // redundant lists for now - DON'T USE !!!
 const wayneList3 = [];
