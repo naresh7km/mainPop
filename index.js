6d8a541afa92c7b40811795b0bc6b0a413710c9d
naresh7km <naresh7km@gmail.com>
2025-02-14 12:41:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const officeList1 = ["seishinyoga"];
+const officeList1 = [];
 const officeList2 = ["hdjavforyou", "javhdaffiliates"];
 const officeList3 = [];
 const officeList4 = [];
 const officeList5 = [];
 
-const dmcList1 = [];
+const dmcList1 = ["seishinyoga"];
 const dmcList2 = ["yogakomorebi.onrender.com"];
 const dmcList3 = [];
 const dmcList4 = [];
