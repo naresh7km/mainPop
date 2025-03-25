148006d18fdea1c4b6b49e74b4a84a311923d120
naresh7km <naresh7km@gmail.com>
2025-03-25 14:05:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const dmcList1 = [];
 
 
 // For tuning ON - dmclist2 
-const dmcList2 = [];
+const dmcList2 = ["matsucafe.onrender.com", "kokorobentoblog.netlify.app", "delightful-smakager-b5ed06.netlify.app"];
 // For tuning OFF - dmclist3
 const dmcList3 = ["ai-miyabi.onrender.com", "seishinyoga", "tokyotastesjp.onrender.com"];
 
@@ -34,7 +34,7 @@ const aomineList4 = [];
 const aomineList5 = [];
 
 // for Turnig Off - list 1 (M)
-const wayneList1 = ["matsucafe.onrender.com", "kokorobentoblog.netlify.app", "delightful-smakager-b5ed06.netlify.app"];
+const wayneList1 = [];
 // for Turning On  - list 2 (O)
 const wayneList2 = [];
 
