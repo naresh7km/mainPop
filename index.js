f7bd18efb2c0ea29e22db8e6f052136601d4100b
naresh7km <naresh7km@gmail.com>
2025-03-28 10:03:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const dmcList3 = ["seishinyoga", "tokyotastesjp.onrender.com"];
 // redundant lists for now - DON'T USE !!!
 const dmcList4 = [];
 const dmcList5 = [];
-const aomineList1 = ["newomnifoodss", "nursefood"];
+const aomineList1 = ["nursefood"];
 const aomineList2 = [];
 const aomineList3 = [];
 const aomineList4 = [];
@@ -36,7 +36,7 @@ const aomineList5 = [];
 // for Turnig Off - list 1 (M)
 const wayneList1 = ["matsucafe.onrender.com", "kokorobentoblog.netlify.app", "delightful-smakager-b5ed06.netlify.app"];
 // for Turning On  - list 2 (O)
-const wayneList2 = [];
+const wayneList2 = ["newomnifoodss"];
 
 // redundant lists for now - DON'T USE !!!
 const wayneList3 = [];
