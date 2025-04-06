5a27f67f2d12dc29f91f263d465f94f49c34c4d6
naresh7km <naresh7km@gmail.com>
2025-04-06 15:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const aomineList4 = [];
 const aomineList5 = [];
 
 // for Turnig Off - list 1 (M)
-const wayneList1 = ["matsucafe.onrender.com", "kokorobentoblog.netlify.app", "delightful-smakager-b5ed06.netlify.app"];
+const wayneList1 = [];
 // for Turning On  - list 2 (O)
-const wayneList2 = [];
+const wayneList2 = ["matsucafe.onrender.com", "kokorobentoblog.netlify.app", "delightful-smakager-b5ed06.netlify.app"];
 
 // redundant lists for now - DON'T USE !!!
 const wayneList3 = [];
