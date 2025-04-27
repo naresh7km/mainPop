e0af2826b3b64056984b5e055e057293a54ccd1e
naresh7km <naresh7km@gmail.com>
2025-04-27 08:49:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const dmcList1 = ["seishinyoga"];
 
 
 // For tuning ON - dmclist2 
-const dmcList2 = ["ai-miyabi.onrender.com", "tokyotastesjp.onrender.com", "yogakomorebi.onrender.com"];
+const dmcList2 = [];
 // For tuning OFF - dmclist3
 const dmcList3 = [];
 
