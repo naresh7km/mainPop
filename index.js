369aa55b000804d5e882c61e26c67b4c23b7fa8d
naresh7km <naresh7km@gmail.com>
2025-05-10 11:34:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const aomineList4 = [];
 const aomineList5 = [];
 
 // for Turnig Off - list 1 (M)
-const aomineList1 = ["newomnifoodss"];
+const aomineList1 = [];
 // for Turning On  - list 2 (O)
-const wayneList2 = ["conversationseattle.shop"];
+const wayneList2 = ["conversationseattle.shop", "newomnifoodss"];
 
 // redundant lists for now - DON'T USE !!!
 const wayneList1 = [];
