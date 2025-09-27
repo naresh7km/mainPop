d65798b114686688dc476dfc96cc21ac9b02e76e
naresh7km <naresh7km@gmail.com>
2025-09-27 09:22:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const officeList1 = ["hdjavforyou", "javhdaffiliates"];
+const officeList1 = [];
 
 // For Turning OFF
 const dmcList1 = ["seishinyoga"];
 
 
-// For OFFICE 
+// For OFFICE
 const dmcList3 = [];
 
 
-// LUPIN
+// For LUPIN 
 const dmcList2 = [];
 
 
@@ -25,7 +25,9 @@ const dmcList2 = [];
 
 
 
-// Redundant lists
+
+// redundant lists for now - DON'T USE !!!
+
 const dmcList4 = [];
 const dmcList5 = [];
 const aomineList2 = [];
@@ -34,22 +36,20 @@ const aomineList4 = [];
 const aomineList5 = [];
 
 // for Turnig Off - list 1 (M)
-const aomineList1 = ["kochifoodie.shop"];
+const aomineList1 = [];
 // for Turning On  - list 2 (O)
-const wayneList2 = ["conversationseattle.shop"];
+const wayneList2 = [];
 
 // redundant lists for now - DON'T USE !!!
-const wayneList1 = [];
+const wayneList1 = ["rafalhomecleaning.onrender.com"];
 const wayneList3 = [];
 const wayneList4 = [];
 const wayneList5 = [];
 
 // Base domains for allowed origins and referrers
 const baseDomains = [
-  "kansaigourmet.shop",
   "seishinyoga.com",
-  "yogayaka.com",
-  "sunblue.yoga",
+  "rafalhomecleaning.onrender.com",
 ];
 
 const generateAllowedUrls = (domains) => {
