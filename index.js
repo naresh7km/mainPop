a7668e1d8aba78c4efa7ca0a6139a72204930f7e
naresh7km <naresh7km@gmail.com>
2025-11-12 11:41:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 
 // For Turning OFF
-const dmcList1 = ["seishinyoga"];
+const dmcList1 = ["seishin"];
 
 
 // For OFFICE
@@ -49,7 +49,7 @@ const wayneList5 = [];
 
 // Base domains for allowed origins and referrers
 const baseDomains = [
-  "seishinyoga.com",
+  "seishin.yoga",
   "rafalhomecleaning.onrender.com",
 ];
 
